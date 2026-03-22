996463d469938d1c4f507df0c99e577093870cc7
Gilbert Bagus N <gilbertbagus24@gmail.com>
2026-03-23 00:11:39 +0700

    Simplify output in main loop
    Removed conditional check and simplified output.

--- Kaboomrico.cpp
+++ Kaboomrico.cpp
@@ -1,12 +1,11 @@
+#include <iostream>
 #include <unistd.h>
 using namespace std;
 
 int main() {
     while(1) {
         fork();
-      if(1){
-          cout << "I LOVE YOU\n";
-      }
+        cout << "I LOVE YOU\n";
     }
     return 0;
 }
